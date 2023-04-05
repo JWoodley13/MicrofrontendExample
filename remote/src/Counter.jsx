f6cdac433c6ea581cfdb6e39978b30c36a670b94
Jordan Woodley <41514105+JWoodley13@users.noreply.github.com>
2023-04-05 18:03:13 +0100

    initial commit

---
+++ remote/src/Counter.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+export default() => {
+    const [count, setCount] = useState(0);
+
+    return (
+        <div>
+            <div>Count = {count}</div>
+            <button onClick={() => setCount(count + 1)}>Add One</button>
+        </div>
+    )
+}
